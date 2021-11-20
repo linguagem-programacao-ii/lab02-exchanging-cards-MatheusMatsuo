388e304b95a11fb124bdd324c485c4bbf002a09d
Matheus <matheus.matsuo@edu.unifil.br>
2021-11-19 23:35:14 -0300

    Trabalho Finalizado

---
+++ lab02/Cartas.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Cartas{
+    public static void TrocarCartas(List<Integer> list1, List<Integer> list2){
+        for (int i=0; i<list1.length; i++){
+            for (int j=0; j<list2.length; j++){
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int aux;
+        List<Integer> listAlice;
+        List<Integer> listBetty;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe quantas cartas Alice tem e Betty tem:");
+        aux = sc.nextInt();
+
+        aux = sc.nextInt();
+
+    }
+}
